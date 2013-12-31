c47a806586bccab6de63c5542e2cadc1c6ae4c8b
IgorMBorushek <igor.borushek@gmail.com>
2013-12-31 13:12:27 -0200

    refactor

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -12,32 +12,34 @@ object Main {
 }
 
   // Exercise 1
+  // Generates a stream which build's Pascal's Triangle to be used by func pascal.
   // I could also do this without the 1s at the beginning and end of Vectors
   // since that is checked for in pascal
-  // but this way the stream completes Pascal's Triangle
+  // but this way the stream completes Pascal's Triangle.
+  // This is at top-level so it can cache.
   lazy val pStream: Stream[Vector[Int]] = {
+    // Helper function to build the vectors according to Pascal's rules.
+    def nextVec(prevVec: Vector[Int], prevLen: Int): Vector[Int] = {
+      lazy val newVec = nextVec(prevVec, prevLen-1) ++ 
+    		  			Vector(prevVec(prevLen-2) + prevVec(prevLen-1))
+      if (prevLen == prevVec.length)
+        newVec ++ Vector(1)
+      else if (prevLen > 1)
+        newVec
+      else Vector(1)
+      }
+    // Builds the vector.  
     Vector(1) #:: Vector(1,1) #:: 
       pStream.zip(pStream.tail).map { pair => 
-        nextVector(pair._2) 
+        nextVec(pair._2, (pair._2).length) 
     	}
   }
   
-  def nextVector(lastVec: Vector[Int]) = {
-      val vec1 = Vector(1)
-      def buildVector(lastLen: Int): Vector[Int] = {
-        if (lastLen == lastVec.length)
-          buildVector(lastLen-1) ++ Vector(lastVec(lastLen-2) + lastVec(lastLen-1)) ++ vec1
-        else if (lastLen > 1)
-          buildVector(lastLen-1) ++ Vector(lastVec(lastLen-2) + lastVec(lastLen-1))
-        else vec1
-      }
-      buildVector(lastVec.length)   
-  }
-  
+  // calls a specific column and row in pascal's triangle.
   def pascal(c: Int, r: Int): Int = {
     require(c >= 0, "column must be >= 0")
 	require(r >= 0, "row must be >= 0")
-	// Can set up more rules like below but the problem asks for recursion
+	// Can set up more rules like below but the problem asks for recursion.
 	if (c == 0 || c == r) 1 else pStream(r)(c) 
   }
 		  
@@ -56,7 +58,7 @@ object Main {
       if (coins.isEmpty || money < coins.min) sum
       else if (coins contains money) 
    	    tailRec(money, coins diff List(money), sum + 1)
-   	  // Sum up the coin combinations using tail recursive calls
+   	  // Sum up the coin combinations using tail recursive calls.
       else coins.filter(money > _).foldLeft (sum) ((foldedSum, coin) => 	
       	     tailRec(money - coin, coins diff (coins.filter (coin < _)), foldedSum)) 
     }
